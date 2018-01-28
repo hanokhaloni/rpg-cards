ecff923e07fe40a181cce7bb02fea3ec1a44f5cb
Henry <hanokh.aloni@gmail.com>
2018-01-28 20:28:58 +0200

    Convert function is bound and assumed to work OK

--- convertor/js/convertor.js
+++ convertor/js/convertor.js
@@ -1,22 +1,92 @@
+var card = 
+{
+    "count": 1,
+    "color": "black",
+    "title": "title",
+    "icon": "white-book-1",
+    "icon_back": "robe",
+    "contents": [
+        "subtitle | 1st level evocation",
+        "rule",
+        "property | Casting time | 1 action",
+        "property | Range | Self (15ft cone)",
+        "property | Components | V,S",
+        "rule",
+        "fill | 2",
+        "text | Each creature in a 15-foot cone must make a Dexterity saving throw. A creature takes <b>3d6 fire damage</b> on a failed save, or half as much damage on a successful one.",
+        "text | The fire ignites any flammable objects in the area that aren't being worn or carried.",
+        "fill | 3",
+        "section | At higher levels",
+        "text | +1d6 damage for each slot above 1st"
+    ],
+    "tags": [],
+    fromCSV: function(values) {
+        this.count = values[0];
+        this.title = values[1];
+        this.icon = values[2];
+        this.icon_back = values[2];
+        this.contents = "text |" + values[3];
+        return JSON.parse(JSON.stringify(this));//TODO extract clone method
+    },
+    getHeader: function() {
+        return [
+            "count",
+            "title",
+            "icon",
+            "contents"];
+
+    },
+}
+
+
 // Ugly global variable holding the current card deck
 
 var validate = function() {
     alert("validate");
 }
 
-var convert = function() {
-    alert("convert");
-    alertInputText();
+var convert = function(data) {
+    console.log("starting Convert...");
+    var data = getDataFromInputField();
+    var cards = processData(data);
+    $("#ouputTextArea").val(JSON.stringify(cards, null, '\t'));
+    console.log("Convert done");
 }
+
+
 
 var alertInputText = function()
 {
-    var val = $.trim($("#inputTextArea").val());
+    var val = getDataFromInputField();
     if (val != "") {
         alert(val);
     }
 
 }
+
+function getDataFromInputField() {
+    return $.trim($("#inputTextArea").val());
+}
+
+function processData(data) {
+    var lines = data.split(/\r\n|\n/);
+
+    //Set up the data arrays
+    var cards = [];
+
+    var headings = lines[0].split(','); // Splice up the first row to get the headings
+
+    for (var j=1; j<lines.length; j++) {
+       var values = lines[j].split(','); 
+       var card1 = card.fromCSV(values);
+       cards.push(card1); 
+
+    }
+    console.log("Successfully proccessed " + cards.length + " cards.");
+    return cards;
+}
+
+
 
 
 $(document).ready(function () {
